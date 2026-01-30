ac59dfc9bb1f4f55009d19be622eb4476254a4fc
EshantBagwe <eshantbagwe2000@gmail.com>
2026-01-30 22:04:44 +0530

    Initial commit madlibs python project

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# madlibs game
+
+adj1 = input("enter an adj1(description): ")
+noun1 = input("enter a noun (person, place, thing): ")
+adj2 = input("enter an adj2(description): ")
+verb1 = input("enter a verb ending with: ")
+adj3 = input("enter an adj3(description): ")
+
+print(f"Today i went to a {adj1} office.")
+print(f"In an exhibit, I saw {noun1}")
+print(f"{noun1} was {adj2} and {verb1}")
+print(f"I was {adj3}!")
